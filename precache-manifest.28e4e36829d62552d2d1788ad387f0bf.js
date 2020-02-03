2af63719bd403a71e1a6fcb3f364075153bf68c3
Hannes O <hannesottt@gmail.com>
2020-02-03 22:02:29 +0200

    Updates

---
+++ precache-manifest.28e4e36829d62552d2d1788ad387f0bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c6c8f6eaa6a63dfe8c110ad6a0b417",
+    "url": "/github-display/index.html"
+  },
+  {
+    "revision": "3e2870d3e1ba0fa47b47",
+    "url": "/github-display/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "77c1885445bbf3c7f325",
+    "url": "/github-display/static/css/main.356b44fd.chunk.css"
+  },
+  {
+    "revision": "3e2870d3e1ba0fa47b47",
+    "url": "/github-display/static/js/2.458079e4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github-display/static/js/2.458079e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77c1885445bbf3c7f325",
+    "url": "/github-display/static/js/main.91b1d21b.chunk.js"
+  },
+  {
+    "revision": "882909c15801946772fc",
+    "url": "/github-display/static/js/runtime-main.2944feca.js"
+  }
+]);
